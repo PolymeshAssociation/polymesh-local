37e0f4e56d6e9ab9c2eb323b574e30aff871d82a
Eric <ericrichardson@polymath.network>
2021-10-06 15:49:04 -0400

    fix: 🐛 Fix bad import path for consts
    Import path contained /src/

--- src/common/chain.ts
+++ src/common/chain.ts
@@ -1,5 +1,5 @@
-import { chain } from '../../src/consts';
 import { returnsExpectedStatus } from '../common/util';
+import { chain } from '../consts';
 
 export async function isChainUp(): Promise<boolean> {
   return returnsExpectedStatus(`http://${chain.url}`, 400);
